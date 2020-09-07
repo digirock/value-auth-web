9baec26371da98336a69b6667bd234335b69d1ef
Shumpei Kobayashi <skonb@me.com>
2020-09-07 17:43:28 +0900

    Settings view

--- src/views/SecuritySettingsView.vue
+++ src/views/SecuritySettingsView.vue
@@ -11,7 +11,7 @@
             <form action="#" v-on:submit.prevent="commitLoginThreshold">
               <dd>
                 <div class="va_select">
-                  <select v-model="lockThreshold">
+                  <select v-model.number="lockThreshold">
                     <option v-for="thresh in lockThresholds" :value="thresh">{{ thresh }}</option>
                   </select>
                 </div>
@@ -27,9 +27,10 @@
             <form action="#" v-on:submit.prevent="commitSecurityLevel">
               <dd>
                 <ul>
-                  <li v-for="l in levels">
-                    <input type="radio" name="level" v-model="level" :value="l" :id="'level' + l"><label
-                      :for="'level' + l">{{ l }}</label>
+                  <li v-for="level in levels">
+                    <input type="radio" name="level" v-model.number="securityLevel" :value="level"
+                           :id="'level' + level"><label
+                      :for="'level' + level">{{ level }}</label>
                   </li>
                 </ul>
                 <input type="submit" value="登録">
@@ -45,28 +46,85 @@
 <script lang="ts">
 import ContentWrapper from "@/components/ContentWrapper.vue";
 import {Component, Vue} from 'vue-property-decorator'
+import {CustomerSetting} from "@/client/ApiResult";
+import BaseView from "@/views/BaseView.vue";
+import {GetCustomerSettingEndpoint, PutCustomerSettingEndpoint} from "@/client/ApiEndpoint";
+import {PutCustomerSettingInput} from "@/client/ApiInput";
 
 @Component({
   components: {ContentWrapper}
 })
-export default class SecuritySettingsView extends Vue {
-  level: number = 0;
+export default class SecuritySettingsView extends BaseView {
   levels: Array<number> = [0, 1, 2, 3, 4, 5,];
-  lockThreshold = 5;
   lockThresholds: Array<number> = [5, 10, 15, 20];
+  _lockThreshold?: number;
+  _securityLevel?: number;
 
-  reloadCurrentSetting() {
-    //TODO implement
+  setting: CustomerSetting = {};
+
+  reload() {
+    this.reloadCurrentSetting();
   }
 
-  commitLoginThreshold() {
+  reloadCurrentSetting(loader?: any) {
+    loader = loader ? loader : this.showLoading();
+    this.apiClient.process({}, GetCustomerSettingEndpoint).then(result => {
+      this.setting = result.results.customer_setting;
+      this.lockThreshold = this.setting.max_attempts;
+      this.securityLevel = this.setting.security_level;
+      loader.hide();
+    }).catch(reason => {
+      this.handleErrors(reason, loader);
+    })
+  }
 
-    //TODO implement
+  get lockThreshold(): number | undefined {
+    return this.setting?.max_attempts;
+  }
+
+  set lockThreshold(val: number | undefined) {
+    this._lockThreshold = val;
+  }
+
+  get securityLevel(): number | undefined {
+    return this.setting?.security_level;
+  }
+
+  set securityLevel(val: number | undefined) {
+    this._securityLevel = val;
+  }
+
+
+  commitLoginThreshold() {
+    if (this._lockThreshold) {
+      let input = <PutCustomerSettingInput>{
+        max_attempts: this._lockThreshold,
+        security_level: this.setting.security_level
+      }
+      let loader = this.showLoading();
+      this.apiClient.process(input, PutCustomerSettingEndpoint).then(result => {
+        this.setting = result.results.customer_setting;
+        this.reloadCurrentSetting(loader);
+      }).catch(reason => {
+        this.handleErrors(reason, loader);
+      })
+    }
   }
 
   commitSecurityLevel() {
-
-    //TODO implement
+    if (this._securityLevel) {
+      let input = <PutCustomerSettingInput>{
+        max_attempts: this.setting.max_attempts,
+        security_level: this._securityLevel
+      }
+      let loader = this.showLoading();
+      this.apiClient.process(input, PutCustomerSettingEndpoint).then(result => {
+        this.setting = result.results.customer_setting;
+        this.reloadCurrentSetting(loader);
+      }).catch(reason => {
+        this.handleErrors(reason, loader);
+      })
+    }
   }
 }
 </script>
